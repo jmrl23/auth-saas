5ab4d8524627a248b8894dbbf7a25f0423197e82
Jomariel Gaitera <jojogaitera@gmail.com>
2024-06-24 18:11:23 +0800

    Serverless attempt #3

---
+++ src/app.serverless.ts
@@ -0,0 +1,20 @@
+import fastifyPlugin from 'fastify-plugin';
+import { NotFound } from 'http-errors';
+import app from './app';
+import setupPlugin from './plugins/setup.plugin';
+import logger from './lib/util/logger';
+
+export default fastifyPlugin(async function appServerless() {
+  app.register(setupPlugin, { prefix: '/' });
+
+  app.setNotFoundHandler(async function notFoundHandler(request) {
+    throw new NotFound(`Cannot ${request.method} ${request.url}`);
+  });
+
+  app.setErrorHandler(async function errorHandler(error) {
+    if (!error.statusCode || error.statusCode > 499) {
+      logger.error(error.stack);
+    }
+    return error;
+  });
+});
--- src/serverless.ts
+++ src/serverless.ts
@@ -1,27 +1,8 @@
 import fastify from 'fastify';
-import fastifyPlugin from 'fastify-plugin';
-import logger from './lib/util/logger';
-import setupPlugin from './plugins/setup.plugin';
-import { NotFound } from 'http-errors';
 
 const app = fastify();
 
-app.register(
-  fastifyPlugin(async function () {
-    app.register(setupPlugin, { prefix: '/' });
-
-    app.setNotFoundHandler(async function notFoundHandler(request) {
-      throw new NotFound(`Cannot ${request.method} ${request.url}`);
-    });
-
-    app.setErrorHandler(async function errorHandler(error) {
-      if (!error.statusCode || error.statusCode > 499) {
-        logger.error(error.stack);
-      }
-      return error;
-    });
-  }),
-);
+app.register(import('./app.serverless'));
 
 export default async function (request: Request, response: Response) {
   await app.ready();
