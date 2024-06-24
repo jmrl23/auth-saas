1f8b8b36ccf8ddfeaf3e830d73a16edd2b7976c5
Jomariel Gaitera <jojogaitera@gmail.com>
2024-06-24 18:07:32 +0800

    Serverless attempt #2

--- src/serverless.ts
+++ src/serverless.ts
@@ -1,4 +1,27 @@
-import app from './app';
+import fastify from 'fastify';
+import fastifyPlugin from 'fastify-plugin';
+import logger from './lib/util/logger';
+import setupPlugin from './plugins/setup.plugin';
+import { NotFound } from 'http-errors';
+
+const app = fastify();
+
+app.register(
+  fastifyPlugin(async function () {
+    app.register(setupPlugin, { prefix: '/' });
+
+    app.setNotFoundHandler(async function notFoundHandler(request) {
+      throw new NotFound(`Cannot ${request.method} ${request.url}`);
+    });
+
+    app.setErrorHandler(async function errorHandler(error) {
+      if (!error.statusCode || error.statusCode > 499) {
+        logger.error(error.stack);
+      }
+      return error;
+    });
+  }),
+);
 
 export default async function (request: Request, response: Response) {
   await app.ready();
